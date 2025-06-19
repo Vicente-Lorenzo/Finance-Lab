cbcb9c6f24b438455c3d4cb6549d266a24d70d67
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-06-19 18:18:16 +0100

    Implemented DDPG Actor class
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

---
+++ Library/Models/DDPG/Actor.py
@@ -0,0 +1,61 @@
+import numpy as np
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from Library.Models.Network.Network import NetworkAPI
+
+class ActorAPI(NetworkAPI):
+
+    def __init__(self,
+                 name: str,
+                 broker: str,
+                 group: str,
+                 symbol: str,
+                 timeframe: str,
+                 alpha,
+                 input_shape,
+                 fc1_shape,
+                 fc2_shape,
+                 actions_shape):
+
+        self.input_shape = input_shape
+        self.fc1_shape = fc1_shape
+        self.fc2_shape = fc2_shape
+        self.actions_shape = actions_shape
+
+        self.fc1 = nn.Linear(*self.input_shape, self.fc1_shape)
+        self.fc2 = nn.Linear(self.fc1_shape, self.fc2_shape)
+
+        self.bn1 = nn.LayerNorm(self.fc1_shape)
+        self.bn2 = nn.LayerNorm(self.fc2_shape)
+
+        self.mu = nn.Linear(self.fc2_shape, self.actions_shape)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+
+        super().__init__(name=name, broker=broker, group=group, symbol=symbol, timeframe=timeframe)
+
+    def init(self):
+        f1 = 1. / np.sqrt(self.fc1.weight.data.size()[0])
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f2 = 1. / np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        f3 = 0.003
+        self.mu.weight.data.uniform_(-f3, f3)
+        self.mu.bias.data.uniform_(-f3, f3)
+
+    def forward(self, state):
+        action = self.fc1(state)
+        action = self.bn1(action)
+        action = F.relu(action)
+        action = self.fc2(action)
+        action = self.bn2(action)
+        action = F.relu(action)
+        action = T.tanh(self.mu(action))
+        return action
