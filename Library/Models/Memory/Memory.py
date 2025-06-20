3f06b92c1c6f3afb1dc8285c9eb6aed368c10f3f
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-06-20 22:59:49 +0100

    Minor method refactor
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Models/Memory/Memory.py
+++ Library/Models/Memory/Memory.py
@@ -14,7 +14,7 @@ class MemoryAPI:
         self._next_state_memory: np.ndarray = np.zeros((self._size, *input_shape))
         self._terminal_memory: np.ndarray = np.zeros(self._size, dtype=np.bool)
 
-    def memorise(self, state, action, reward, next_state, done):
+    def memorise(self, state, action, reward, next_state, done) -> None:
         index = self._counter % self._size
         self._state_memory[index] = state
         self._action_memory[index] = action
@@ -23,11 +23,9 @@ class MemoryAPI:
         self._terminal_memory[index] = done
         self._counter += 1
 
-    def remember(self, batch_size):
+    def remember(self, batch_size) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray):
         max_mem = min(self._counter, self._size)
-
         batch = np.random.choice(max_mem, batch_size)
-
         states = self._state_memory[batch]
         actions = self._action_memory[batch]
         rewards = self._reward_memory[batch]
