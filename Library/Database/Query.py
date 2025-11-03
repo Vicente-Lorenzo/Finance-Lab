b260dc1a0ab443beca6a888a4ec249d43b3ffb6b
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-11-03 17:44:34 +0000

    [Database] Improved QueryAPI
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Database/Query.py
+++ Library/Database/Query.py
@@ -3,10 +3,7 @@ from Library.Utility import PathAPI
 class QueryAPI:
 
     def __init__(self, data: str | PathAPI):
-        self._query = data(frame=2).read_text() if isinstance(data, PathAPI) else data
+        self._query: str = data(frame=2).read_text() if isinstance(data, PathAPI) else data
 
     def __call__(self, **kwargs) -> str:
         return self._query.format(**kwargs)
-
-    def __repr__(self) -> str:
-        return self._query
