942b13c2f00e1f3ed2bc266b9ed2c1639fed43b4
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-11-03 17:27:48 +0000

    [Utility] Improved PathAPI
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Utility/Path.py
+++ Library/Utility/Path.py
@@ -1,24 +1,34 @@
 from sys import _getframe
 from pathlib import Path
 
+def inspect_file(file: str | Path) -> Path:
+    return Path(file).resolve()
+
+def inspect_file_path(file: str | Path) -> str:
+    return str(inspect_file(file))
+
+def inspect_module(file: str | Path) -> Path:
+    return inspect_file(file).parent
+
+def inspect_module_path(file: str | Path) -> str:
+    return inspect_file_path(inspect_module(file))
+
+def traceback_file(frame: int) -> Path:
+    return inspect_file(traceback_file_path(frame))
+
+def traceback_file_path(frame: int) -> str:
+    return _getframe(frame).f_code.co_filename
+
+def traceback_module(frame: int) -> Path:
+    return inspect_module(traceback_file_path(frame))
+
+def traceback_module_path(frame: int) -> str:
+    return inspect_module_path(traceback_file_path(frame))
+
 class PathAPI:
+    def __init__(self, file: str):
+        self.file = file
 
-    def __init__(self, path: str, anchor: str = None):
-        anchor = anchor if anchor else _getframe(1).f_code.co_filename
-        self.module = Path(anchor).resolve().parent
-        self.path = self.module / path
-
-    def exists(self) -> bool:
-        return self.path.exists()
-
-    def read_text(self, **kwargs) -> str:
-        return self.path.read_text(**kwargs)
-
-    def read_bytes(self) -> bytes:
-        return self.path.read_bytes()
-
-    def write_bytes(self, data: bytes):
-        self.path.write_bytes(data)
-
-    def write_text(self, data: str, **kwargs):
-        self.path.write_text(data, **kwargs)
+    def __call__(self, anchor: str = None, frame: int = 1) -> Path:
+        anchor = anchor if anchor else traceback_file_path(frame=frame)
+        return inspect_module(anchor) / self.file
