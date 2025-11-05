4f1017949a51e333fa7d23571a093bbc7613fea8
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-11-05 16:01:36 +0000

    [Utility] Improved PathAPI
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Utility/Path.py
+++ Library/Utility/Path.py
@@ -1,5 +1,8 @@
 from sys import _getframe
 from pathlib import Path
+from dataclasses import dataclass, field
+
+from Library.Dataclass import DataclassAPI
 
 def inspect_file(file: str | Path) -> Path:
     return Path(file).resolve()
@@ -13,23 +16,28 @@ def inspect_module(file: str | Path) -> Path:
 def inspect_module_path(file: str | Path) -> str:
     return inspect_file_path(inspect_module(file))
 
-def traceback_file(frame: int) -> Path:
+def traceback_file(frame: int = None) -> Path:
     return inspect_file(traceback_file_path(frame))
 
-def traceback_file_path(frame: int) -> str:
-    return _getframe(frame).f_code.co_filename
+def traceback_file_path(frame: int = None) -> str:
+    return _getframe(frame if frame is not None else 3).f_code.co_filename
 
-def traceback_module(frame: int) -> Path:
+def traceback_module(frame: int = None) -> Path:
     return inspect_module(traceback_file_path(frame))
 
-def traceback_module_path(frame: int) -> str:
+def traceback_module_path(frame: int = None) -> str:
     return inspect_module_path(traceback_file_path(frame))
 
-class PathAPI:
+@dataclass(slots=True)
+class PathAPI(DataclassAPI):
 
-    def __init__(self, file: str):
-        self.file = file
+    File: str = field(init=True, repr=True)
+    Anchor: str = field(default=None, init=True, repr=True)
 
-    def __call__(self, anchor: str = None, frame: int = 1) -> Path:
-        anchor = anchor if anchor else traceback_file_path(frame=frame)
-        return inspect_module(anchor) / self.file
+    Module: Path = field(init=False, repr=True)
+    Path: Path = field(init=False, repr=True)
+
+    def __post_init__(self):
+        self.Anchor = self.Anchor if self.Anchor is not None else traceback_file_path()
+        self.Module = inspect_module(self.Anchor)
+        self.Path = self.Module / self.File
