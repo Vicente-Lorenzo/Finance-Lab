06a159bfd48b44abdbc63ff79e71f229c9a147e3
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-11-16 19:22:18 +0000

    [Database] Implemented Table and Structure queries
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Database/Check/Table.sql
+++ Library/Database/Check/Table.sql
@@ -1,8 +1,3 @@
-WITH expected_structure(column_name, data_type) AS (VALUES {definitions}),
-actual_structure AS (SELECT column_name, data_type FROM information_schema.columns WHERE table_schema = '{schema}' AND table_name = '{table}')
-SELECT e.column_name, e.data_type, a.column_name, a.data_type
-FROM expected_structure e
-FULL OUTER JOIN actual_structure a
-    ON e.column_name = a.column_name
-    AND e.data_type = a.data_type
-WHERE a.column_name IS NULL OR e.column_name IS NULL;
+SELECT 1
+FROM information_schema.tables
+WHERE table_schema = '{schema}' AND table_name   = '{table}';
---
+++ Library/Database/Structure/Table.sql
@@ -0,0 +1,19 @@
+WITH expected_structure(column_name, data_type) AS (
+    VALUES {definitions}
+),
+actual_structure AS (
+    SELECT column_name, data_type
+    FROM information_schema.columns
+    WHERE table_schema = '{schema}'
+      AND table_name   = '{table}'
+)
+SELECT
+    e.column_name AS expected_column_name,
+    e.data_type   AS expected_data_type,
+    a.column_name AS actual_column_name,
+    a.data_type   AS actual_data_type
+FROM expected_structure e
+FULL OUTER JOIN actual_structure a
+  ON e.column_name = a.column_name
+ AND e.data_type   = a.data_type
+WHERE a.column_name IS NULL OR e.column_name IS NULL;
