7e5a14ed194c5bec68c5ac5f971dff03eda3b463
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-12-05 19:26:05 +0000

    [Logging] Implemented File Logging
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Logging/__init__.py
+++ Library/Logging/__init__.py
@@ -1,10 +1,12 @@
 from Library.Logging.Logging import VerboseLevel, LoggingAPI
 from Library.Logging.Console import ConsoleLoggingAPI
 from Library.Logging.Buffer import BufferLoggingAPI
+from Library.Logging.File import FileLoggingAPI
 
 __all__ = [
     "VerboseLevel",
     "LoggingAPI",
     "ConsoleLoggingAPI",
-    "BufferLoggingAPI"
+    "BufferLoggingAPI",
+    "FileLoggingAPI"
 ]
