94e8f9e6fc3e970378a2586ba3501fb3938ae919
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2025-12-07 16:38:00 +0000

    [Logging] Implemented Report Logging
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

---
+++ Library/Logging/Report.py
@@ -0,0 +1,56 @@
+from abc import ABC
+from Library.Logging import VerboseLevel, LoggingAPI
+
+class ReportLoggingAPI(LoggingAPI, ABC):
+
+    _SUCCESS_TAG_ = "[✔️Success✔️]"
+    _FAILURE_TAG_ = "[❌Failure❌]"
+
+    _class_success_flag_ = None
+    _class_failure_flag_ = None
+    _class_verbose_threshold_: VerboseLevel = None
+
+    @classmethod
+    def _setup_class_(cls) -> None:
+        cls.set_verbose_level(VerboseLevel.Silent, default=True)
+        cls.set_threshold_level(VerboseLevel.Exception)
+        cls.disable_success_report()
+        cls.enable_failure_report()
+        cls.enable_logging()
+
+    @classmethod
+    def is_success_report_enabled(cls) -> bool:
+        return cls._class_success_flag_
+
+    @classmethod
+    def enable_success_report(cls) -> None:
+        cls._class_success_flag_ = True
+
+    @classmethod
+    def disable_success_report(cls) -> None:
+        cls._class_success_flag_ = False
+
+    @classmethod
+    def is_failure_report_enabled(cls) -> bool:
+        return cls._class_failure_flag_
+
+    @classmethod
+    def enable_failure_report(cls) -> None:
+        cls._class_failure_flag_ = True
+
+    @classmethod
+    def disable_failure_report(cls) -> None:
+        cls._class_failure_flag_ = False
+
+    @classmethod
+    def set_threshold_level(cls, threshold_verbose: VerboseLevel) -> None:
+        cls._class_verbose_threshold_ = threshold_verbose
+
+    @classmethod
+    def is_success_report(cls) -> bool:
+        if not cls.is_success_report_enabled(): return False
+        return cls._verbose_min_ and cls._verbose_min_.value > cls._class_verbose_threshold_.value
+
+    def is_failure_report(self) -> bool:
+        if not self.is_failure_report_enabled(): return False
+        return self._verbose_min_ and self._verbose_min_.value <= self._class_verbose_threshold_.value
