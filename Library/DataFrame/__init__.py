64c074ca7b744862144ac875cb6f4aefb4cb34a8
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-01-23 17:54:13 +0000

    [DataFrame] Minor fixes to bounds
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/DataFrame/__init__.py
+++ Library/DataFrame/__init__.py
@@ -1,6 +1,5 @@
-import sys
 import numpy as np
-np.set_printoptions(threshold=sys.maxsize, linewidth=sys.maxsize)
+np.set_printoptions(threshold=1000, linewidth=1000)
 import pandas as pd
 pd.options.display.max_rows = None
 pd.options.display.max_columns = None
@@ -9,8 +8,8 @@ pd.options.display.max_colwidth = None
 import polars as pl
 pl.Config.set_tbl_cols(-1)
 pl.Config.set_tbl_rows(-1)
-pl.Config.set_tbl_width_chars(sys.maxsize)
-pl.Config.set_fmt_str_lengths(sys.maxsize)
+pl.Config.set_tbl_width_chars(1000)
+pl.Config.set_fmt_str_lengths(1000)
 pl.Config.set_fmt_table_cell_list_len(-1)
 
 __all__ = [
