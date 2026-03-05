a4ed9fdcbfc4893e7bd80e88d4594f4bede016f9
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-03-05 22:38:15 +0000

    [App] Minor Fixes
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/App/App.py
+++ Library/App/App.py
@@ -567,10 +567,8 @@ class AppAPI:
                                     original_args=args,
                                     mode=mode
                                 )
-
                     on_app_init = getattr(func, "_on_app_init_", Injection.Disabled)
                     kwargs["prevent_initial_call"] = Injection.coerce(on_app_init) is Injection.Disabled
-
                     running = getattr(func, "_callback_running_", None)
                     if running:
                         kwargs["running"] = [
@@ -579,14 +577,12 @@ class AppAPI:
                             else item
                             for item in running
                         ]
-
                     cancel = getattr(func, "_callback_cancel_", None)
                     if cancel:
                         kwargs["cancel"] = [
                             item.build(context) if isinstance(item, Trigger) else item
                             for item in cancel
                         ]
-
                     progress = getattr(func, "_callback_progress_", None)
                     if progress:
                         if isinstance(progress, list):
@@ -598,15 +594,12 @@ class AppAPI:
                             kwargs["progress"] = progress.build(context)
                         else:
                             kwargs["progress"] = progress
-
                     interval = getattr(func, "_callback_interval_", None)
                     if interval is not None:
                         kwargs["interval"] = interval
-
                     progress_default = getattr(func, "_callback_progress_default_", None)
                     if progress_default is not None:
                         kwargs["progress_default"] = progress_default
-
                     args = [a.build(context=context) if isinstance(a, Trigger) else a for a in args]
                     if is_client:
                         self.app.clientside_callback(target, *args, **kwargs)
